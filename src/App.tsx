10c9ef709417cd7b4fe6a7cde5c3650b599ee460
Amit Kushwaha <amitcoding56@gmail.com>
2025-07-27 14:54:59 +0530

    Initial Setup

---
+++ src/App.tsx
@@ -0,0 +1,16 @@
+import "./App.css";
+import Navbar from "./components/Common/Navbar";
+import Footer from "./components/Common/footer";
+
+function App() {
+  return (
+    <>
+      <div>
+        <Navbar />
+        <Footer />
+      </div>
+    </>
+  );
+}
+
+export default App;
